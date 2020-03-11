aaa34233721b77bc93f97d11faaa9c438983e959
Genna Mergola <gennamergola@gmail.com>
2020-03-10 20:11:44 -0700

    almost there. can't get gif on the page due to cookies

--- app.js
+++ app.js
@@ -3,21 +3,23 @@ console.log("Let's get this party started!");
 
 async function getGif(){
     let searchValue = $("input").val();
+    // let searchValue = "joke"
     let gif = await axios.get(
-        "http://api.giphy.com/v1/gifs/search", 
-        {params: {q:searchValue, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
-
-    // console.log(gif.data.data)
-    appendGif(gif.data.data[0][0].images.downsized_large.url)
+        "http://api.giphy.com/v1/gifs/search", {params: {q:searchValue, api_key: "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym"}})
+    return  gif.data.data[0].images.original.url;
 }
 
-function appendGif(url){
-    let gifImage = $("<image>")
-        .css("src",  url)
+$("form").on("submit", async function appendGif(evt){
+    evt.preventDefault();
+    let theUrl = await getGif();
+    let gifDiv = $("<div>")
+        .css("background-image",  `url(${theUrl})`)
 
-    $(".gifs").append(gifImage)
-}
+    $("#gifs").append(gifDiv)
 
-$(function(){
-    $("#search").on("click", getGif)
+    $(function(){
+        $("#search").on("click", appendGif)
+    })
 })
+
+
